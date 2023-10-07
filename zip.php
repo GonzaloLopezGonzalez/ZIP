9c2ea663f2e7f58c1a8eec859f8808c4e0fdc8fc
Gonzalo López <lopezgonzalez@hotmail.com>
2023-10-07 16:57:30 +0200

    Password compression

--- zip.php
+++ zip.php
@@ -1,82 +1,87 @@
 <?php
 
-class Zip
-{
-    private $zip;
-    private $zipName = '';
+class Zip{
 
-    public function __construct(string $zipName)
-    {
-        $this->checkIfZipModuleIsLoaded();
-        $this->checkZipExtensionInZipName($zipName);
-        $this->zipName = $zipName;
-        $this->zip = new ZipArchive();
-        $this->zip->open($this->zipName, ZipArchive::CREATE);
+  private $zip;
+  private $zipName = '';
+
+  public function __construct(string $zipName)
+  {
+     $this->checkIfZipModuleIsLoaded();
+     $this->checkZipExtensionInZipName($zipName);
+     $this->zipName = $zipName;
+     $this->zip = new ZipArchive();
+
+     $this->zip->open($this->zipName,ZipArchive::CREATE);
+  }
+
+  private function checkIfZipModuleIsLoaded()
+  {
+    if(!extension_loaded('zip')){
+      throw new Exception('No está cargado el módulo de ficheros zip');
     }
+  }
 
-    private function checkIfZipModuleIsLoaded()
-    {
-        if (!extension_loaded('zip')) {
-            throw new Exception('No está cargado el módulo de ficheros zip');
+  private function checkZipExtensionInZipName(string $zipName)
+  {
+    if (strpos($zipName, 'zip') === false){
+       throw new Exception('El nombre del fichero tiene que tener extension .zip');
+    }
+  }
+
+  public function addFile(string $fileName, $password = null)
+  {
+    $this->zip->addFile($fileName,basename($fileName));
+    if (!empty($password)){
+      $this->zip->setEncryptionName(basename($fileName), ZipArchive::EM_AES_256, $password);
+    }
+  }
+
+  public function addFiles(array $arrFileNames, $password = null)
+  {
+    $notExistingFiles = '';
+    foreach ($arrFileNames as $fileName){
+      if (file_exists($fileName)){
+          $this->addFile($fileName, $password);
+      }else{
+        $notExistingFiles .= $fileName.',';
+      }
+    }
+    $notExistingFiles = rtrim($notExistingFiles,',');
+    if (str_word_count($notExistingFiles) > 0 ){
+        throw new Exception('Los siguientes ficheros no existen: '.$notExistingFiles.' y no se han comprimido');
+    }
+  }
+
+  public function addFilesByFolder(string $folder, $password = null)
+  {
+     if(is_dir($folder)){
+        if (substr($folder, -1) !== '/'){
+          $folder .= '/';
         }
+        $options = array('add_path' => "$folder", 'remove_all_path' => TRUE);
+        $this->zip->addGlob("$folder*.*", GLOB_BRACE, $options);
+     }else{
+         throw new Exception('No existe la ruta.');
     }
+  }
 
-    private function checkZipExtensionInZipName($zipName)
-    {
-        if (strpos($zipName, 'zip') === false) {
-            throw new Exception('El nombre del fichero tiene que tener extension .zip');
+  public function extractTo(string $extractPath, $password = null)
+  {
+    if ($this->zip->open($this->zipName) === TRUE) {
+      if (!is_dir($extractPath)){
+          mkdir($extractPath, 0755, true);
+      }
+      if (empty($password) OR $this->zip->setPassword($password)){
+        if(!$this->zip->extractTo($extractPath)){
+          throw new Exception('Password incorrecta.');
         }
+      }
     }
+  }
 
-    public function addFile(string $fileName)
-    {
-        $this->zip->addFile($fileName, $fileName);
-    }
-
-    public function addFiles(array $arrFileNames)
-    {
-        $notExistingFiles = '';
-        foreach ($arrFileNames as $fileName) {
-            if (file_exists($fileName)) {
-                $this->addFile($fileName);
-            } else {
-                $notExistingFiles .= $fileName.',';
-            }
-        }
-        $fileName = rtrim($fileName, ',');
-        if (str_word_count($fileName) > 0) {
-            throw new Exception('Los siguientes ficheros no existen: '.$notExistingFiles.' y no se han comprimido');
-        }
-    }
-
-    public function addFilesByFolder($folder)
-    {
-        if (is_dir($folder)) {
-            if (substr($folder, -1) !== '/') {
-                $folder .= '/';
-            }
-            $this->zip->addEmptyDir($folder);
-            $list = glob("$folder*.*");
-            foreach ($list as $fileName) {
-                $this->addFile($fileName);
-            }
-        } else {
-            throw new Exception('No existe la ruta.');
-        }
-    }
-
-    public function extractTo($extractPath)
-    {
-        if ($this->zip->open($this->zipName) === true) {
-            if (!is_dir($extractPath)) {
-                mkdir($extractPath, 0755, true);
-            }
-            $this->zip->extractTo($extractPath);
-        }
-    }
-
-    public function __destruct()
-    {
-        $this->zip->close();
-    }
+  public function __destruct()
+  {
+    $this->zip->close();
+  }
 }
